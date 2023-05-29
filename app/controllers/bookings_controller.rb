9a1c3273f828300e31174b4873eb1e89766da593
Alejandro Leon <79841123+Alexleon2411@users.noreply.github.com>
2023-05-29 16:24:35 +0200

    routing created

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,29 @@
 class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to index_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def acept
+    @status
+  end
+
+  def decline
+    @status
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :card_id, :status, :start_date, :end_date)
+  end
 end
