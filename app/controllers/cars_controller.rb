045d06682d4c5ae1eeb1cee9b3337075dc0b2b83
Alejandro Leon <79841123+Alexleon2411@users.noreply.github.com>
2023-05-29 17:52:59 +0200

    controller update car

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,11 +1,12 @@
 class CarsController < ApplicationController
   before_action :set_car, only: [:show, :destroy, :update, :edit]
+
   def index
     @cars = Car.all
+
     if params[:query].present?
       @query = params[:query]
       @cars = Car.where("name LIKE ?", "%#{params[:query]}%")
-
     else
       @cars = Car.all
     end
@@ -21,7 +22,7 @@ class CarsController < ApplicationController
   def create
     @car = Car.new(car_params)
     if @car.save
-      redirect_to cars_path
+      redirect_to car_path(@car)
     else
       render :new, status: :unprocessable_entity
     end
@@ -32,7 +33,7 @@ class CarsController < ApplicationController
 
   def update
     if @car.update(car_params)
-      redirect_to cars_path
+      redirect_to car_path(@car)
     else
       render :edit
     end
@@ -44,10 +45,12 @@ class CarsController < ApplicationController
   end
 
   private
+
   def set_car
     @car = Car.find(params[:id])
   end
 
   def car_params
     params.require(:car).permit(:model, :price, :city, :year, :user_id)
+  end
 end
