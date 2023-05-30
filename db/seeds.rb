2e292c27bbe567f075ae084d6ddfdc94b6fe3fc4
mustanser ali <razaali199913@outlook.com>
2023-05-30 11:54:06 +0200

    seed file done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,28 @@ require "faker"
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+User.destroy_all
+
+# creqte a few users
+
+user1 = User.new(
+  email: "alpha@gmail.com",
+  password: "123456",
+  password_confirmation: "123456"
+)
+user1.save!
+
+user2 = User.new(
+  email: "beta@gmail.com",
+  password: "123456",
+  password_confirmation: "123456"
+)
+user2.save!
+
 puts 'intiallizing seeding.....'
-30.times do
+10.times do
   new_cars = Car.new(model: Faker::Vehicle.make, price: Faker::Commerce.price(range: 1000..10_000, as_string: true), city: Faker::Address.city, year: Faker::Vehicle.year)
+  new_cars.user = [user1, user2].sample
   new_cars.save!
 end
 puts "seeding successfull"
