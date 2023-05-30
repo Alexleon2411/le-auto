6ad679173664e363e03e3e3c93ee61eef448b193
Alejandro Leon <79841123+Alexleon2411@users.noreply.github.com>
2023-05-30 23:09:32 +0200

    working withu the index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-<<<<<<< HEAD
-
-=======
->>>>>>> 3fe99aa7a2be7f44ef188cea374068aa4de2eb09
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :photo])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :photo])
